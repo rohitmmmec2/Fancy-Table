172da975eabc8bcae71de7ddf999ae8f13e1ebbb
rohitmmmec2 <you@example.com>
2023-10-15 13:38:30 +0530

    corrected first version

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 function App() {
 
-  const people = [{id: 12, name: 'Creola Katherine Johnson', profession: 'mathematician'},
+  const people = [{id: 0, name: 'Creola Katherine Johnson', profession: 'mathematician'},
                  {id: 1, name: 'Mario José Molina-Pasquel Henríquez', profession: 'chemist'},
                  {id: 2, name: 'Mohammad Abdus Salam', profession: 'physicist'},
                  {id: 3, name: 'Percy Lavon Julian', profession: 'chemist'},
